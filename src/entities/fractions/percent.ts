273270f9baf93ed8184a09a8a0f08012cb925bd8
halfalicious <nilserik.frantzell@gmail.com>
2021-05-13 16:03:14 -0700

    Fix bug in percent math

--- src/entities/fractions/percent.ts
+++ src/entities/fractions/percent.ts
@@ -1,14 +1,34 @@
-import { Rounding, _100 } from '../../constants'
+import { Rounding, _100, BigintIsh } from '../../constants'
 import { Fraction } from './fraction'
 
 const _100_PERCENT = new Fraction(_100)
 
 export class Percent extends Fraction {
   public toSignificant(significantDigits: number = 5, format?: object, rounding?: Rounding): string {
-    return this.multiply(_100_PERCENT).toSignificant(significantDigits, format, rounding)
+    return super.multiply(_100_PERCENT).toSignificant(significantDigits, format, rounding)
   }
 
   public toFixed(decimalPlaces: number = 2, format?: object, rounding?: Rounding): string {
-    return this.multiply(_100_PERCENT).toFixed(decimalPlaces, format, rounding)
+    return super.multiply(_100_PERCENT).toFixed(decimalPlaces, format, rounding)
+  }
+
+  public add(other: Percent | BigintIsh): Percent {
+    const result = super.add(other);
+    return new Percent(result.numerator, result.denominator);
+  }
+
+  public subtract(other: Percent | BigintIsh): Percent {
+    const result = super.subtract(other);
+    return new Percent(result.numerator, result.denominator);
+  }
+
+  public multiply(other: Percent | BigintIsh): Percent {
+    const result = super.multiply(other);
+    return new Percent(result.numerator, result.denominator);
+  }
+
+  public divide(other: Percent | BigintIsh): Percent {
+    const result = super.divide(other);
+    return new Percent(result.numerator, result.denominator);
   }
 }
